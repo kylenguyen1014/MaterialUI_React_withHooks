a34aafa81d30e4b460a54386fed6e3c703b4277e
Trung Nguyen <43191329+kylenguyen1014@users.noreply.github.com>
2019-09-16 19:27:07 -0400

    refactor SinglePalette styles

--- src/DraggableBox.js
+++ src/DraggableBox.js
@@ -3,7 +3,7 @@ import { SortableElement} from 'react-sortable-hoc';
 import { withStyles } from '@material-ui/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
 import IconButton from '@material-ui/core/IconButton';
-import styles from './styles/DraggableBox';
+import styles from './styles/DraggableBoxStyles';
 
 const DraggableBox = SortableElement((props) => {
     const { classes, name, background} = props;
---
+++ src/styles/SinglePaletteStyles.js
@@ -0,0 +1,45 @@
+import down from './ResponsiveHelper';
+
+const styles = {
+    root: {
+        // height: '90vh',
+        width: '100%',
+        overflow: 'hidden',
+    },
+    container: {
+        height: '90vh',
+        width: '100%',
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    goBack:{
+        width: '20%',
+        height: '50%',
+        backgroundColor: '#000',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'white',
+        cursor: 'pointer',
+        [down('lg')]:{
+            width: '75%',
+            height: '33.33%',
+        },
+        [down('md')]:{
+            width: '50%',
+            height: '20%',
+        },
+        [down('sm')]:{
+            width: '100%',
+            height: '10%',
+        },
+        '& span':{
+            opacity: '0',
+            transition: 'opacity 0.4s ease-in-out',
+        },
+        '&:hover span': {
+            opacity: '1',
+        }
+    }
+}
+ export default styles;
